187ef2132095d0c55261ad97762bb7254f7ba01f
Muhammad Tayyab <tayyabsultan621@gmail.com>
2025-04-03 11:49:41 +0500

    dads

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://vue-jobs-backend-ochre.vercel.app/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
