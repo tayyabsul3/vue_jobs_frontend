84105c34ef747e51ef2c93cc503b1a8107c3a112
Muhammad Tayyab <tayyabsultan621@gmail.com>
2025-04-03 11:55:21 +0500

    dasdadasdsa

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -20,7 +20,7 @@ const state = reactive({
 
 onMounted(async () => {
   try {
-    const response = await axios.get('/api/jobs');
+    const response = await axios.get('https://vue-jobs-backend-ochre.vercel.app/jobs');
     state.jobs = response.data;
   } catch (error) {
     console.error('Error fetching jobs', error);
